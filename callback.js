ebe96f0c4d0c8b9a2c896deea304751be5b8b045
Rama116 <ramagugan77@gmail.com>
2024-09-14 12:30:22 +0530

    async func

--- callback.js
+++ callback.js
@@ -10,41 +10,64 @@
 
 //     setTimeout(one,3000,"Luffy")
 
-function goat()
-{
-    console.log("He is the GOAT")
-}
-function notgoat()
-{
-    console.log("He isnt the GOAT")
-}
-function error()
-{
-    console.log("Absolutely not")
-}
+// function goat()
+// {
+//     console.log("He is the GOAT")
+// }
+// function notgoat()
+// {
+//     console.log("He isnt the GOAT")
+// }
+// function error()
+// {
+//     console.log("Absolutely not")
+// }
 
-function exec(data,callback)
-{
-    if(data)
+// function exec(data,callback)
+// {
+//     if(data)
+//     {
+//         console.log(`Player : ${data}`)
+//     }
+//     // callback()
+//     setTimeout(callback,2000)
+// }
+
+// function check(data){
+//         if(data==="Ronaldo")
+//         {
+//             exec(data,goat)
+//         }
+//         else if(data==="Messi")
+//         {
+//             exec(data,notgoat)
+//         }
+//         else{
+//             exec("He's shit",error)
+//         }
+// }
+
+// check("Ronaldo")
+
+async function API(){
+    try{
+        const r = await fetch('https://66e526ff5cc7f9b6273c6cfb.mockapi.io/register')
+        const d = await r.json()
+        // console.table(d)
+        return d
+    }catch(error)
     {
-        console.log(`Player : ${data}`)
+        console.log(error)
     }
-    // callback()
-    setTimeout(callback,2000)
 }
 
-function check(data){
-        if(data==="Ronaldo")
-        {
-            exec(data,goat)
-        }
-        else if(data==="Messi")
-        {
-            exec(data,notgoat)
-        }
-        else{
-            exec("He's shit",error)
-        }
+// need to print like this when it is not printed in API function
+async function APID()
+{
+    const r = await API()
+    console.table(r)
+    const structureddatamap = r.map((data) => {console.log(data)})
+    console.table(structureddatamap)
 }
 
-check("Ronaldo")
+APID()
